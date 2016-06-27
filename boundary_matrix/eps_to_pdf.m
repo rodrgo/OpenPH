049dc03b47272b68c4c210c5b4c9a092e7c9dce7
Rodrigo Mendoza-Smith <mendozasmith@maths.ox.ac.uk>
2016-06-27 11:22:09 +0100

    Staged removed files for commit

--- boundary_matrix/eps_to_pdf.m
+++
@@ -1,8 +0,0 @@
-
-function eps_to_pdf(file_path)
-    status = system('export LC_CTYPE=en_US.UTF-8');
-    status = system('export LC_ALL=en_US.UTF-8');
-
-    system_command = ['find . -wholename "' file_path '" -exec epstopdf {} ";"'];
-    status = system(system_command);
-end
