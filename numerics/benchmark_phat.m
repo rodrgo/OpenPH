cecab29ea9b1b39ffac39d27b3235e7281a7c904
Rodrigo Mendoza <eserod@gmail.com>
2019-01-08 00:06:32 +0000

    phat table fix

--- numerics/benchmark_phat.m
+++ numerics/benchmark_phat.m
@@ -2,17 +2,6 @@
 init;
 plot_init;
 
-%folder = '../datasets/pointclouds';
-%fid = fopen(fullfile('pointclouds_stream_success.list'), 'r');
-%tline = fgetl(fid);
-
-%[r, c, m] = dat2cmo(fullfile('single_triangle.dat'));
-%low = phat(r, c, m, PHAT_DIR);
-%low
-
-%[lows, t] = std_red_testing(R);
-%lows
-
 % Complex parameters
 MAX_DIM = 20;
 num_points = 5:19;
@@ -26,7 +15,9 @@ cparams.num_points    = 5;
 
 shape = 'random_gaussian';
 
-num_algos   = 2;
+phat_algos = {'twist', 'chunk', 'spectral_sequence', 'row'};
+
+num_algos   = 1+2*length(phat_algos);
 num_shapes  = 1;
 num_samples = 3;
 COL_WIDTH   = 8;
@@ -36,8 +27,6 @@ ts  = zeros(num_algos, length(num_points));
 nps = zeros(1, length(num_points));
 ms  = zeros(1, length(num_points));
 colors = create_color_palette(num_algos);
-
-phat_algos = {'standard', 'twist', 'chunk', 'chunk_sequential', 'spectral_sequence', 'row'};
 
 for j = 1:length(num_points)
     np = num_points(j);
@@ -49,32 +38,32 @@ for j = 1:length(num_points)
     ms(j) = m;
 
     test_tic = tic;
-    [OUT, t] = openph(r, c, m, 'pms', COL_WIDTH, zeros(1,m));
+    %[OUT, t] = openph(r, c, m, 'pms', COL_WIDTH, zeros(1,m));
+    [lows, t] =  std_red_testing(full(sparse(r, c, ones(size(r)), m, m)));
     ts(1, j) = toc(test_tic)
-    lows = OUT.low;
+    %lows = OUT.low;
 
     for k = 1:length(phat_algos)
         % Without dualization
         [lows_phat, t] = phat(r, c, m, PHAT_DIR, phat_algos{k}, false);
         ts(1+k, j) = t;
+        assert(sum(lows ~= lows_phat) == 0);
 
         % With dualization
         [lows_phat, t] = phat(r, c, m, PHAT_DIR, phat_algos{k}, true);
         ts(1+length(phat_algos)+k, j) = t;
+        assert(sum(lows ~= lows_phat) == 0);
     end
 
-    assert(sum(lows ~= lows_phat) == 0);
 end 
-
 
 % =============
 % Create figure
 % =============
 
-table_tag = strcat('phat_benchmark.tex');
+table_tag = strcat('table_phat.tex');
 table_path = fullfile(FIGURE_DIR, table_tag);
 
-% fopen
 fileId = fopen(table_path, 'w');
 
 fprintf(fileId,'\\begin{tabular}{l l ||');
@@ -87,7 +76,7 @@ fprintf(fileId,'\\toprule\n');
 fprintf(fileId,'\\multirow{2}{*}{$N$} & \n');
 fprintf(fileId,'\\multirow{2}{*}{$m$} & \n');
 for k = 1:length(phat_algos)
-    fprintf(fileId,'\\multicolumn{2}{c}{A (\%)}');
+    fprintf(fileId,'\\multicolumn{2}{c}{%s}', strrep(phat_algos{k}, '_', '-'));
     if k < length(phat_algos)
         fprintf(fileId,'& \n');
     else
@@ -95,9 +84,9 @@ for k = 1:length(phat_algos)
     end
 end
 
-fprintf(fileId,'& \n');
+fprintf(fileId,'& ');
 for k = 1:length(phat_algos)
-    fprintf(fileId,' & p & d ');
+    fprintf(fileId,' & {P} & {D} ');
 end
 fprintf(fileId,'\\\\\n');
 fprintf(fileId,'\\midrule\n');
@@ -105,12 +94,12 @@ fprintf(fileId,'\\midrule\n');
 for j = 1:length(nps)
     fprintf(fileId,'%d & %d', nps(j), ms(j));
     for k = 1:length(phat_algos)
-        fprintf(fileId, '& %3.6f & %3.6f', ts(1+k,j), ts(1+length(phat_algos)+k,j));
+        fprintf(fileId, '& %d & %d', round(ts(1+k,j)*1000, 0), round(ts(1+length(phat_algos)+k,j)*1000, 0));
     end
     fprintf(fileId, '\\\\\n');
 end 
 fprintf(fileId,'\\bottomrule\n');
-fprintf(fileId,'\\{tabular}');
+fprintf(fileId,'\\end{tabular}');
 
 % fclose
 fclose(fileId);
