d43ef2caa2a2fccb12de815b8fe81b96a8ea567d
Rodrigo Mendoza <eserod@gmail.com>
2019-01-08 17:15:31 -0600

    setup file

--- src/cuda/setup.sh
+++ src/cuda/setup.sh
@@ -1,9 +1,23 @@
-
+#!/bin/sh
 # Dirty hack to automatically get matlab path
-MATLAB_PATH=$(matlab -nosplash -nodesktop -r "try; f=matlabroot; fprintf(2,'%s',f); catch; end; exit" > /dev/null 2 | sed -e 's/^[ \t\n\r]*//')
+MATLAB_PATH=$(matlab -nosplash -nodesktop -r "try; f=matlabroot; fprintf('%s:',f); catch; end; exit" | tail -n1 | awk -F: '{print $1}')
+echo "export MATLAB=${MATLAB_PATH}" >> config
 
-# Dirty hack to get cuda version
+# CUDAHOME
 CUDA_VERSION=$(nvcc --version | grep -oP '(?<=release )[0-9.]+')
+CUDAHOME=$(whereis "cuda-${CUDA_VERSION}" | awk '{print $2}')
+if [ -z "$CUDAHOME" ]; then
+    CUDAHOME=$(whereis "cuda" | awk '{print $2}')
+fi
+echo "export CUDAHOME=${CUDAHOME}" >> config
+
+# SM
+CUDA_SM=$(python sm.py)
+echo "export GPUARCH=${CUDA_SM}" >> config
+
+# CUDAMATLAB
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+echo "CUDAMATLAB=${BASEDIR}CudaMatlab" >> config
 
 # Get nvmex and nvopts.sh
 wget http://developer.download.nvidia.com/compute/cuda/1_1/Matlab_Cuda_1.1.tgz
@@ -12,12 +26,3 @@ mv Matlab_Cuda_1.1/nvmex CudaMatlab
 mv Matlab_Cuda_1.1/nvopts.sh CudaMatlab
 rm -rf Matlab_Cuda_1.1
 rm Matlab_Cuda_1.1.tgz
-
-# Create config file
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "export TDA=${DIR}/cuda" >> config
-echo "export MATLAB=${MATLAB_PATH}" >> config
-echo "export CUDAHOME=/usr/local/cuda-${CUDA_VERSION}" >> config
-echo "export GPUARCH=sm_61" >> config
-echo "CUDAMATLAB=${DIR}/cuda/CudaMatlab" >> config
-
