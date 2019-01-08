6f90ae417adedeb9216faa8e5f3475176ef45a41
Rodrigo Mendoza <eserod@gmail.com>
2019-01-08 17:11:11 -0600

    sm

--- src/cuda/sm.py
+++ src/cuda/sm.py
@@ -2,31 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Outputs some information on CUDA-enabled devices on your computer,
-including current memory usage.
-
-It's a port of https://gist.github.com/f0k/0d6431e3faa60bffc788f8b4daa029b1
-from C to Python with ctypes, so it can run without compiling anything. Note
-that this is a direct translation with no attempt to make the code Pythonic.
-It's meant as a general demonstration on how to obtain CUDA device information
-from Python without resorting to nvidia-smi or a compiled Python extension.
-
-Author: Jan Schlüter
-
-# Modified by RMS
+Adapted from Jan Schlüter's code in
+https://gist.github.com/f0k/0d6431e3faa60bffc788f8b4daa029b1
 """
 
 import sys
 import ctypes
 
-# Some constants taken from cuda.h
-CUDA_SUCCESS = 0
-CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT = 16
-CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR = 39
-CU_DEVICE_ATTRIBUTE_CLOCK_RATE = 13
-CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE = 36
+def main():
 
-def get_sm():
+    # Some constants taken from cuda.h
+    CUDA_SUCCESS = 0
+    CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT = 16
+    CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR = 39
+    CU_DEVICE_ATTRIBUTE_CLOCK_RATE = 13
+    CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE = 36
+
+    # Find libnames
     libnames = ('libcuda.so', 'libcuda.dylib', 'cuda.dll')
     for libname in libnames:
         try:
@@ -37,7 +29,6 @@ def get_sm():
             break
     else:
         raise OSError("could not load any of: " + ' '.join(libnames))
-    sms = {}
 
     nGpus = ctypes.c_int()
     name = b' ' * 100
@@ -52,27 +43,30 @@ def get_sm():
     result = cuda.cuInit(0)
     if result != CUDA_SUCCESS:
         cuda.cuGetErrorString(result, ctypes.byref(error_str))
-        print("cuInit failed with error code %d: %s" % (result, error_str.value.decode()))
+        print("cuInit failed with error code %d: %s" % (result, error_str.value.decode()), file=sys.stderr)
         return 1
     result = cuda.cuDeviceGetCount(ctypes.byref(nGpus))
     if result != CUDA_SUCCESS:
         cuda.cuGetErrorString(result, ctypes.byref(error_str))
-        print("cuDeviceGetCount failed with error code %d: %s" % (result, error_str.value.decode()))
+        print("cuDeviceGetCount failed with error code %d: %s" % (result, error_str.value.decode()), file=sys.stderr)
         return 1
+
+    sm_codes = {}
     for i in range(nGpus.value):
         result = cuda.cuDeviceGet(ctypes.byref(device), i)
         if result != CUDA_SUCCESS:
             cuda.cuGetErrorString(result, ctypes.byref(error_str))
-            print("cuDeviceGet failed with error code %d: %s" % (result, error_str.value.decode()))
+            print("cuDeviceGet failed with error code %d: %s" % (result, error_str.value.decode()), file=sys.stderr)
             return 1
         if cuda.cuDeviceComputeCapability(ctypes.byref(cc_major), ctypes.byref(cc_minor), device) == CUDA_SUCCESS:
-            sms[i] = "sm_%d%d" % (cc_major.value, cc_minor.value)
-    return sms 
+            sm_codes[i] = "sm_%d%d" % (cc_major.value, cc_minor.value)
 
-def main():
-    sms = get_sms()
-    print(sms)
-
+    # Get sm_code and print
+    sm_code = ''
+    if len(list(set(sm_codes.values()))) > 1:
+        print("More than one SM found. Using GPU-0", file=sys.stderr)
+        sm_code = sm_codes[0]
+    print(sm_code)
     return 0
 
 if __name__=="__main__":
